3b5983bf90a4ee4815288d1957c6ab3753fc1673
lcy <lcy@IFCHIC-MacBook-Pro.local>
2025-01-02 09:08:58 +0800

    Add config assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: isProd ? '/wedding-invitation' : ''
+  basePath: isProd ? '/wedding-invitation' : '',
+  assetPrefix: isProd ? 'https://lcyyyyyyyy.github.io/wedding-invitation' : undefined
 }
 
 export default nextConfig
