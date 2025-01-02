82bdad493c04d4780e6c91771bef3ae84c2b15ba
lcy <lcy@IFCHIC-MacBook-Pro.local>
2025-01-02 09:02:45 +0800

    Modify basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/wedding-invitation'
+  basePath: isProd ? '/wedding-invitation' : ''
 }
 
 export default nextConfig
