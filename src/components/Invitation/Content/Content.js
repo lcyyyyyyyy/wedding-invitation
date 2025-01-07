e1d574cf3cc1b3d51c255f73f893e19dbb977fcc
lcy <lcy@IFCHIC-MacBook-Pro.local>
2025-01-07 15:37:46 +0800

    Remove envelope

--- src/components/Invitation/Content/Content.js
+++ src/components/Invitation/Content/Content.js
@@ -4,7 +4,7 @@
 
 'use client'
 
-import { useRef } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import gsap from 'gsap'
 import Image from 'next/image'
 import ScrollTrigger from 'gsap/ScrollTrigger'
@@ -19,16 +19,26 @@ import KissFemale from '@/components/Icons/KissFemale'
 
 gsap.registerPlugin(ScrollTrigger)
 
-const Content = ({ isOpen }) => {
+const Content = () => {
   const image = '/2X0A4512.jpg'
   const isProd = process.env.NODE_ENV === 'production'
   const rootUrl = process.env.NEXT_PUBLIC_ROOT_URL
   const container = useRef(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    setIsLoaded(true)
+  }, [])
 
   useGSAP(
     () => {
-      if (isOpen) {
-        const tl = gsap.timeline()
+      if (isLoaded) {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            start: 'top bottom-=10%',
+            trigger: `.${styles.wrapper}`
+          }
+        })
 
         tl
           .to(`.${styles.wrapper}`, {
@@ -79,13 +89,13 @@ const Content = ({ isOpen }) => {
           })
       }
     },
-    { scope: container, dependencies: [isOpen] }
+    { scope: container, dependencies: [isLoaded] }
   )
 
   return (
     <div
       ref={container}
-      className={`${styles.container}${isOpen ? ` ${styles.open}` : ''}`}
+      className={styles.container}
     >
       <div className={styles.wrapper}>
         <div className={styles.image}>
@@ -115,10 +125,12 @@ const Content = ({ isOpen }) => {
                   />
                 </div>
               </h4>
-              <h5>傅</h5>
+              <h5>－ 男方家長 －</h5>
               <h5>傅金寶 ． 陳秀貞</h5>
+              <h5>－ 祖父母 －</h5>
+              <h5>傅阿松 ． 傅高金妹</h5>
             </div>
-            <span className={styles.and}>&</span>
+            {/* <span className={styles.and}>&</span> */}
             <div className={styles.block}>
               <h4>
                 丞映
@@ -130,8 +142,10 @@ const Content = ({ isOpen }) => {
                   />
                 </div>
               </h4>
-              <h5>林</h5>
+              <h5>－ 女方家長 －</h5>
               <h5>林錦祥 ． 陳麗蓮</h5>
+              <h5>－ 祖父母 －</h5>
+              <h5>林溪川 ． 林謝玉蘭</h5>
             </div>
           </div>
           <div className={styles.info}>
--- src/components/Invitation/Invitation.js
+++ src/components/Invitation/Invitation.js
@@ -4,23 +4,14 @@
 
 'use client'
 
-import { useState } from 'react'
-
 import styles from './Invitation.module.scss'
 
 import Content from './Content/Content'
-import Envelope from './Envelope/Envelope'
 
 const Invitation = () => {
-  const [isOpen, setIsOpen] = useState(false)
-
   return (
     <div className={styles.wrapper}>
-      <Content isOpen={isOpen} />
-      <Envelope
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-      />
+      <Content />
     </div>
   )
 }
