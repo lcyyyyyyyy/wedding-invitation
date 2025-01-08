4828377170a64f6193947b36ef7c381f79f01ad0
lcy <lcy@IFCHIC-MacBook-Pro.local>
2025-01-08 10:23:30 +0800

    Add config basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: '/wedding-invitation',
+  assetPrefix: '/wedding-invitation/'
+}
 
 export default nextConfig
