15ec7d4a33252e20ccce33c9e4c27670fd47b60d
lcy <lcy@IFCHIC-MacBook-Pro.local>
2025-02-05 15:19:03 +0800

    Add drawing

--- src/components/Date/Date.js
+++ src/components/Date/Date.js
@@ -6,6 +6,7 @@
 
 import { useRef, useState, useEffect } from 'react'
 import gsap from 'gsap'
+import Image from 'next/image'
 import { useGSAP } from '@gsap/react'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 
@@ -17,6 +18,9 @@ import Lovestruck from '../Icons/Lovestruck'
 gsap.registerPlugin(ScrollTrigger)
 
 const Date = () => {
+  const image = '/Drawing.webp'
+  const isProd = process.env.NODE_ENV === 'production'
+  const rootUrl = process.env.NEXT_PUBLIC_ROOT_URL
   const container = useRef(null)
   const [dates, setDates] = useState([])
   const [isLoaded, setIsLoaded] = useState(false)
@@ -34,24 +38,39 @@ const Date = () => {
   useGSAP(
     () => {
       if (isLoaded) {
-        const tl = gsap.timeline({
+        let tl = gsap.timeline({
           scrollTrigger: {
-            start: 'top bottom-=20%',
-            trigger: `.${styles.wrapper}`,
-            onEnter: () => {
-              document.querySelector(`.${styles.wrapper} path`).classList.add(styles.draw)
-            }
+            trigger: `.${styles.wrapper}`
           }
         })
 
         tl
-          .to(`.${styles.wrapper} .${styles.icon} svg`, {
+          .to(`.${styles.wrapper} h2`, {
+            ease: 'power4.inOut',
+            duration: 0.6,
+            clipPath: 'polygon(0 0%, 100% 0%, 100% 100%, 0% 100%)'
+          })
+          .to(`.${styles.image}`, {
+            ease: 'power4.inOut',
+            opacity: 1,
+            duration: 0.6
+          })
+
+        gsap
+          .to(`.${styles.date} .${styles.icon} svg`, {
             y: 0,
             x: 0,
             ease: 'power4.inOut',
             delay: 0.6,
             opacity: 1,
-            duration: 1
+            duration: 1,
+            scrollTrigger: {
+              start: 'top bottom-=20%',
+              trigger: `.${styles.date}`,
+              onEnter: () => {
+                document.querySelector(`.${styles.date} path`).classList.add(styles.draw)
+              }
+            }
           })
       }
     },
@@ -61,58 +80,69 @@ const Date = () => {
   return (
     <div ref={container} className={styles.container}>
       <div className={styles.wrapper}>
-        <h2>
-          2025.3.8
-          <Line
-            size={200}
-            color='#222222'
+        <h2>Save The Date!</h2>
+        <div className={styles.image}>
+          <Image
+            alt={image}
+            src={isProd ? `${rootUrl}${image}` : image}
+            fill={true}
+            sizes='100vw'
           />
-        </h2>
-        <div className={styles.td}>
-          <p>一</p>
-          <p>二</p>
-          <p>三</p>
-          <p>四</p>
-          <p>五</p>
-          <p>六</p>
-          <p>日</p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          {dates?.map(date => {
-            return (
-              <p
-                key={date}
-                style={date === 8 ? { color: '#fff' } : {}}
-              >
-                {date === 8 &&
-                  <span className={styles.icon}>
-                    <Lovestruck
-                      size={60}
-                      fill='#df3a3a'
-                      color='#222222'
-                    />
-                  </span>
-                }
-                {date}
-              </p>
-            )
-          })}
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
         </div>
-        <h3>
-          中午12:00
-        </h3>
-        <h3>
-          <small>11:30入席</small>
-        </h3>
+        <div className={styles.date}>
+          <h3>
+            2025.3.8
+            <Line
+              size={200}
+              color='#222222'
+            />
+          </h3>
+          <div className={styles.td}>
+            <p>一</p>
+            <p>二</p>
+            <p>三</p>
+            <p>四</p>
+            <p>五</p>
+            <p>六</p>
+            <p>日</p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            {dates?.map(date => {
+              return (
+                <p
+                  key={date}
+                  style={date === 8 ? { color: '#fff' } : {}}
+                >
+                  {date === 8 &&
+                    <span className={styles.icon}>
+                      <Lovestruck
+                        size={60}
+                        fill='#df3a3a'
+                        color='#222222'
+                      />
+                    </span>
+                  }
+                  {date}
+                </p>
+              )
+            })}
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+          <h4>
+            中午12:00
+          </h4>
+          <h4>
+            <small>11:30入席</small>
+          </h4>
+        </div>
       </div>
     </div>
   )
